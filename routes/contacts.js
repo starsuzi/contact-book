dfd2590a0ee3a10f96d542a969b2b247ce5594a1
정소영 <starsuzi@naver.com>
2018-11-12 18:44:57 +0900

    edit completed

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,3 +1,4 @@
+/*
 var express  = require("express");
 var router   = express.Router();
 var Contact  = require("../models/Contact");
@@ -56,3 +57,46 @@ router.delete("/:id", function(req, res){
 });
 
 module.exports = router;
+*/
+var express = require("express");
+var router = express.Router();
+var Contact = require("../models/Contact");
+
+//Index
+router.get("/", function(req, res){
+  Contact.find({}, function (err, contacts) { //contacts는 검색결과
+    if(err) return res.json(err);
+    res.render("contacts/index", {contacts:contacts});
+  });
+});
+
+//New
+router.get("/new", function(req, res){
+  res.render("contacts/new");
+});
+
+//create
+router.post("/", function(req, res){
+  Contact.create(req.body, function(err,contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
+
+//show
+router.get("/:id", function(req, res){
+  Contact.findOne({_id:req.param.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render("contacts/show", {contact:contact});
+  });
+});
+
+//edit
+router.get("/:id/edit",function(req, res){
+  //{_id:req.param.id}는 조건으로 쓰고 있다.
+  //DB의 contacts collection에서 _id가 req.param.id와 같은 경우가 조건
+  Contact.findOneAndUpdate({_id:req.param.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render("contacts/edit", {contact:contact});
+  });
+});
