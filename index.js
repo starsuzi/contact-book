807f70fc04a3be5d13428e1bde7dbb927c35cd41
정소영 <starsuzi@naver.com>
2018-11-15 15:54:22 +0900

    added index, new, create, show, edit, delete

--- index.js
+++ index.js
@@ -37,25 +37,25 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser"); //node.js의 post요청 데이터 추출
 var methodOverride = require("method-override");//put, delete
 var app = express();
+
+// DB setting
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true });
+var db = mongoose.connection;
 db.once("open", function(){
   console.log("DB connected");
 });
-db.on("error", function(){
-  console.log("DB error : ", err);
+db.on("error", function(err){
+  console.log("DB ERROR : ", err);
 });
 
 //other settings
-app.set("view engines", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));//_method으로 들어오는 값을 바꿈
 
-//DB setting
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true});
-var db = mongoose.connection;
-
-//routes
+// Routes
 app.use("/", require("./routes/home"));
 app.use("/contacts", require("./routes/contacts"));
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -66,7 +66,9 @@ var Contact = require("../models/Contact");
 router.get("/", function(req, res){
   Contact.find({}, function (err, contacts) { //contacts는 검색결과
     if(err) return res.json(err);
+    // console.log(contacts);
     res.render("contacts/index", {contacts:contacts});
+
   });
 });
 
@@ -85,8 +87,12 @@ router.post("/", function(req, res){
 
 //show
 router.get("/:id", function(req, res){
-  Contact.findOne({_id:req.param.id}, function(err, contact){
+  console.log(req.params.id);
+  console.log(req.params._id);
+  Contact.findOne({_id:req.params.id}, function(err, contact){
     if(err) return res.json(err);
+    if (contact === null)
+      res.redirect("/contacts");
     res.render("contacts/show", {contact:contact});
   });
 });
@@ -95,8 +101,30 @@ router.get("/:id", function(req, res){
 router.get("/:id/edit",function(req, res){
   //{_id:req.param.id}는 조건으로 쓰고 있다.
   //DB의 contacts collection에서 _id가 req.param.id와 같은 경우가 조건
-  Contact.findOneAndUpdate({_id:req.param.id}, function(err, contact){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
     if(err) return res.json(err);
     res.render("contacts/edit", {contact:contact});
   });
 });
+
+//update
+router.put("/:id/update", function(res,req){
+  //첫번째 parameter는 찾을 조건을 object로 입력
+  //두번째 parameter는 update할 정보를 object로 입력 data를 찾는다
+  //찾은 후 callback함수 호출
+  console.log(req.body);
+  Contact.findOneAndUpdate({_id:req.params.id},req.body, function(err, contact){
+    if(err) return res.json;
+    res.redirect("/contacts/"+req.params.id);
+  });
+});
+
+//destroy
+router.delete("/:id", function(req, res){
+  Contact.remove({_id:req.params.id}, function(err){
+    if(err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
+
+module.exports = router;
